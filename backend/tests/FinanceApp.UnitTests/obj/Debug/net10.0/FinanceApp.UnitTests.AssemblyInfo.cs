24ad21873f17ca6ecde8a8debbd9d5009e6ad77b
Carlos <169368514+cavelasquez24@users.noreply.github.com>
2026-07-06 22:01:21 -0500

    feat: Investments and SavingsGoals modules - CRUD, records and deposit endpoints V2

--- backend/tests/FinanceApp.UnitTests/obj/Debug/net10.0/FinanceApp.UnitTests.AssemblyInfo.cs
+++ backend/tests/FinanceApp.UnitTests/obj/Debug/net10.0/FinanceApp.UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceApp.UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22e59dce96d0349323adc31f228f02f43bb94409")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1880448888fca6dcfb7c35f2ac4f55907815f02e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceApp.UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceApp.UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
